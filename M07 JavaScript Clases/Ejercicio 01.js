5fd3d12bfb5beeaf164bb4ae89cdaf9dddae74a9
CDPATINOCoder <cesardavidpatinovera@gmail.com>
2023-10-16 11:06:26 -0300

    tarea 7 terminada

--- M07 JavaScript Clases/Ejercicio 01.js
+++ M07 JavaScript Clases/Ejercicio 01.js
@@ -10,19 +10,67 @@ function crearUsuario() {
    // El valor {{nombre}} debe ser el nombre definido para la instancia de la clase.
    // Retornar la clase.
    // Tu código:
+   class Usuario
+   {
+      constructor(usuario, nombre, email, password)
+      {
+         this.usuario = usuario;
+         this.nombre = nombre;
+         this.email = email;
+         this.password = password;
+      }
+      saludar()
+      {
+         return 'Hola, mi nombre es ' + this.nombre;
+      }
+   }
+   return Usuario;
 }
+
+var llamarfuncion = new crearUsuario();
+var miUsuarion = new llamarfuncion('cesarpatino24','César Patiño', 'cesardavidpatinovera@gmail.com', 'cesarcrack24');
+console.log(miUsuarion.saludar());
 
 function agregarMetodoPrototype(Usuario) {
    // Agrega un método al prototipo de "Usuario".
    // El método debe llamarse "saludar" y debe devolver el string "Hello World!".
    // Tu código:
+   Usuario.prototype.saludar = function ()
+   {
+      return "Hello World!";
+   }
+   return Usuario;
 }
+
+class miUsuario
+{
+   constructor()
+   {
+      this.nombre = "cesar";
+   }
+}
+
+var prueba = new agregarMetodoPrototype(miUsuario);
+var anashe = new prueba;
+console.log(anashe.saludar());
+console.log(anashe);
+
+
 
 function agregarStringInvertida() {
    // Agrega un método al prototipo de "String" que devuelva la misma cadena de caracteres, pero invertida.
    // El método debe llamarse "reverse".
    // [PISTA]: necesitarás utilizar el objeto "this".
+   String.prototype.reverse = function ()
+   {
+      return this.split('').reverse().join('');
+   };
 }
+agregarStringInvertida();
+var palabra = "hola";
+console.log(palabra.reverse());
+
+
 
 /*⚠️ NO MODIFIQUES NADA DEBAJO DE ESTO ⚠️*/
 module.exports = {
